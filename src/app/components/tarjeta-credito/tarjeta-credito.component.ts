9d5c296325771dff25791388eed66804a8cbb359
Duvan <duvanriano2626@gmail.com>
2025-10-08 20:43:00 -0500

    Se crea el formulario reactivo se asocia el formulario al html y se crear el metodo para cuando el se le de submit al boton

--- src/app/components/tarjeta-credito/tarjeta-credito.component.ts
+++ src/app/components/tarjeta-credito/tarjeta-credito.component.ts
@@ -1,17 +1,49 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormGroup, ReactiveFormsModule,FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-tarjeta-credito',
-  imports: [CommonModule],
+  imports: [CommonModule,ReactiveFormsModule],
   templateUrl: './tarjeta-credito.component.html',
   styleUrl: './tarjeta-credito.component.css'
 })
 export class TarjetaCreditoComponent {
+
   listTarjetas: any[] = [
     { titular: 'Juan Perez', numeroTarjeta: '1234 5678 9012 3456', fechaExpiracion: '12/24', cvv: '123' },
     { titular: 'Maria Lopez', numeroTarjeta: '9876 5432 1098 7654', fechaExpiracion: '11/23', cvv: '456' }
   ];
 
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      titular: [''],
+      numeroTarjeta: [''],
+      fechaExpiracion: [''],
+      cvv: ['']
+    });
+
+  }
+
+  /**
+   * name
+   */
+  public agregarTarjeta() {
+    console.log(this.form.value);
+
+    const nuevaTarjeta = {
+      titular: this.form.get('titular')?.value,
+      numeroTarjeta: this.form.get('numeroTarjeta')?.value,
+      fechaExpiracion: this.form.get('fechaExpiracion')?.value,
+      cvv: this.form.get('cvv')?.value
+    };
+    console.log(nuevaTarjeta);
+
+    this.listTarjetas.push(nuevaTarjeta);
+    this.form.reset();
+
+  }
 
 }
